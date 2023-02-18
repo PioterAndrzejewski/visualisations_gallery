edf556f7917d8b09d84d5154a702d39c3e55abc0
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-02-18 23:49:28 +0100

    prepare gh pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,6 @@ const App = () => {
                 open={index >= 0}
                 index={index}
                 close={() => setIndex(-1)}
-                // enable optional lightbox plugins
                 plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
             />
         </>
